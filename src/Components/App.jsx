ba8643f9db805e6a73a49057da232f97406f1cb0
baimag <baimag02@yandex.ru>
2021-04-07 19:32:20 +0300

    homework

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Base from "./Base";
-import Detective from "./Detective";
 
 function App() {
   return (
     <div className="App">
       <Base />
-      <Detective />
     </div>
   );
 }
